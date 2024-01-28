bb65d29613b974f16feae874589f650e34299df4
Iain Buclaw <ibuclaw@gdcproject.org>
2024-01-28 21:46:53 +0100

    Start the service listening on any address

--- source/app.d
+++ source/app.d
@@ -12,7 +12,7 @@ shared static this()
 
 	auto settings = new HTTPServerSettings;
 	settings.port = to!ushort(environment.get("PORT", "8080"));
-	settings.bindAddresses = ["::1", "127.0.0.1"];
+	settings.bindAddresses = ["::", "0.0.0.0"];
 
     auto router = new URLRouter;
     router.get("/", &indexPage);
